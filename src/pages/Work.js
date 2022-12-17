f9a24c2239e2aa631988f62d2acf5326cf3e0f03
eidoj <dudwns5013@gmail.com>
2022-12-17 16:51:05 +0900

    상세보기 간략한 css

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Work = () => {
   const state = useSelector((state) => state.todoReducer.List);
@@ -9,18 +10,36 @@ const Work = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <span>{work.id}</span>
-      <input
-        type="button"
-        value="이전으로"
-        onClick={() => {
-          navigate("/");
-        }}
-      />
-      <h1>{work.title}</h1>
-      <span>{work.body}</span>
+      <TodoBox>
+        <BoxHead>
+          <span>id:{work.id}</span>
+          <input
+            type="button"
+            value="이전으로"
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+        </BoxHead>
+        <h1>{work.title}</h1>
+        <span>{work.body}</span>
+      </TodoBox>
     </div>
   );
 };
 
 export default Work;
+
+const TodoBox = styled.div`
+  width: 320px;
+  height: 200px;
+  padding: 20px;
+  border: 1px solid;
+  border-radius: 20px;
+`;
+
+const BoxHead = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
